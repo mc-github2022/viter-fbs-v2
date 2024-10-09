4272b66510cb6c644cb75d519b5025816dc79be7
Jhonny Dichoso <jhonny.dichoso@frontlinebusiness.com.ph>
2024-10-09 16:15:21 +0800

    miscellaneous support

--- src/components/pages/website/virtualSolution/vaMiscellaneous/VaMiscellaneousServiceList.jsx
+++ src/components/pages/website/virtualSolution/vaMiscellaneous/VaMiscellaneousServiceList.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { FaHeadset, FaRegHandshake } from "react-icons/fa6";
-import { GoChecklist } from "react-icons/go";
-import {
-  MdInfoOutline,
-  MdOutlineRocketLaunch,
-  MdOutlineSupportAgent,
-} from "react-icons/md";
+import { Captions, Headset, Calculator, ListChecks } from "lucide-react";
 
 const VaMiscellaneousServiceList = () => {
   return (
@@ -14,80 +8,54 @@ const VaMiscellaneousServiceList = () => {
         <div className="customContainer">
           <div className="sectionDesc text-center md:w-[70%] mx-auto mb-14">
             <h2 className="text-[clamp(20px,6vw,60px)] leading-[1.1] font-semibold mb-10">
-              What Can Customer Service Assistants Do?
+              What <span className="text-primary">Comprehensive Support </span>{" "}
+              Can We Perform?
             </h2>
             <p className="subDesc mb-10">
-              Using an assistant to help manage your customer service saves you
-              time and money and means that you can deliver a better customer
-              experience.
+              Our VA for Administrative Assistance offers comprehensive support
+              to streamline your business operations.
             </p>
             <a
               href="#"
-              className="btn bg-primary text-light font-bold hover:bg-secondary transition-all"
+              className="btn bg-primary text-light font-light hover:bg-secondary transition-all"
             >
-              Get Started
+              GET STARTED
             </a>
           </div>
           <ul className="serviceInclusion grid md:grid-cols-2 gap-6">
             <li className="flex items-center gap-7">
               <div className="bg-customGray rounded-lg addShadow">
                 <div className="w-16 h-16 grid place-items-center">
-                  <FaHeadset className="text-3xl text-primary" />
+                  <Captions size={28} className="text-3xl text-primary" />
+                </div>
+              </div>
+              <p className="font-semibold text-xl lg:text-2xl">Transcription</p>
+            </li>
+            <li className="flex items-center gap-7">
+              <div className="bg-customGray rounded-lg addShadow">
+                <div className="w-16 h-16 grid place-items-center">
+                  <Calculator size={28} className="text-3xl text-primary" />
+                </div>
+              </div>
+              <p className="font-semibold text-xl lg:text-2xl">Bookkeeping</p>
+            </li>
+            <li className="flex items-center gap-7">
+              <div className="bg-customGray rounded-lg addShadow">
+                <div className="w-16 h-16 grid place-items-center">
+                  <Headset size={28} className="text-3xl text-primary" />
                 </div>
               </div>
               <p className="font-semibold text-xl lg:text-2xl">
-                Phone, Email & Live Chat Support
+                Back Office Services
               </p>
             </li>
             <li className="flex items-center gap-7">
               <div className="bg-customGray rounded-lg addShadow">
                 <div className="w-16 h-16 grid place-items-center">
-                  <MdInfoOutline className="text-3xl text-primary" />
+                  <ListChecks size={28} className="text-3xl text-primary" />
                 </div>
               </div>
-              <p className="font-semibold text-xl lg:text-2xl">
-                Escalation Management
-              </p>
-            </li>
-            <li className="flex items-center gap-7">
-              <div className="bg-customGray rounded-lg addShadow">
-                <div className="w-16 h-16 grid place-items-center">
-                  <MdOutlineSupportAgent className="text-3xl text-primary" />
-                </div>
-              </div>
-              <p className="font-semibold text-xl lg:text-2xl">
-                24/7 Customer Support
-              </p>
-            </li>
-            <li className="flex items-center gap-7">
-              <div className="bg-customGray rounded-lg addShadow">
-                <div className="w-16 h-16 grid place-items-center">
-                  <FaRegHandshake className="text-3xl text-primary" />
-                </div>
-              </div>
-              <p className="font-semibold text-xl lg:text-2xl">
-                Customer Retention & Relationship Building
-              </p>
-            </li>
-            <li className="flex items-center gap-7">
-              <div className="bg-customGray rounded-lg addShadow">
-                <div className="w-16 h-16 grid place-items-center">
-                  <MdOutlineRocketLaunch className="text-3xl text-primary" />
-                </div>
-              </div>
-              <p className="font-semibold text-xl lg:text-2xl">
-                Customer Onboarding
-              </p>
-            </li>
-            <li className="flex items-center gap-7">
-              <div className="bg-customGray rounded-lg addShadow">
-                <div className="w-16 h-16 grid place-items-center">
-                  <GoChecklist className="text-3xl text-primary" />
-                </div>
-              </div>
-              <p className="font-semibold text-xl lg:text-2xl">
-                Order Processing & Fulfillment
-              </p>
+              <p className="font-semibold text-xl lg:text-2xl">Various Tasks</p>
             </li>
           </ul>
         </div>
