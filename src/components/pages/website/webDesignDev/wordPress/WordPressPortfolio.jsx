afe1d700315de44a76fa63506ffa62ca155df89c
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-10-10 16:02:08 +0800

    added portfolio

--- src/components/pages/website/webDesignDev/wordPress/WordPressPortfolio.jsx
+++ src/components/pages/website/webDesignDev/wordPress/WordPressPortfolio.jsx
@@ -3,11 +3,38 @@ import React from "react";
 const WordPressPortfolio = () => {
   return (
     <>
-      <section className="profolioWebsite">
+      <section className="profolioWebsite py-40">
         <div className="customContainer">
-          <div className="wrapper">
-            <div className="imageItem">
-              <img src="https://placehold.co/300x400" alt="" />
+          <div className="">
+            <p>Seamless performance and stunning design</p>
+            <h2 className="text-[35px] font-semibold text-primary leading-[1.1] mb-8">
+              custom-built website
+            </h2>
+          </div>
+          <div className="wrapper grid grid-rows-6 grid-cols-4 gap-4">
+            <div className="imageItem row-span-4 h-[180]">
+              <img src="" className="w-full h-full object-cover" alt="" />
+            </div>
+            <div className="imageItem  row-span-3 h-[180]">
+              <img src="" className="w-full h-full object-cover" alt="" />
+            </div>
+            <div className="imageItem row-span-6 h-[180]">
+              <img src="" className="w-full h-full object-cover" alt="" />
+            </div>
+            <div className="imageItem row-span-2 h-[180]">
+              <img src="" className="w-full h-full object-cover" alt="" />
+            </div>
+            <div className="imageItem row-span-2 h-[180]">
+              <img src="" className="w-full h-full object-cover" alt="" />
+            </div>
+            <div className="imageItem row-span-3 h-[180]">
+              <img src="" className="w-full h-full object-cover" alt="" />
+            </div>
+            <div className="imageItem row-span-2 h-[180]">
+              <img src="" className="w-full h-full object-cover" alt="" />
+            </div>
+            <div className="imageItem row-span-2 h-[180]">
+              <img src="" className="w-full h-full object-cover" alt="" />
             </div>
           </div>
         </div>
