99ab39c01546f9bc09e573957ba810612100f0af
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-10-10 16:16:05 +0800

    updated

--- src/components/pages/website/webDesignDev/wordPress/WordPressPortfolio.jsx
+++ src/components/pages/website/webDesignDev/wordPress/WordPressPortfolio.jsx
@@ -13,25 +13,49 @@ const WordPressPortfolio = () => {
           </div>
           <div className="wrapper grid grid-rows-6 grid-cols-4 gap-4">
             <div className="imageItem row-span-4 h-[180]">
-              <img src="" className="w-full h-full object-cover" alt="" />
+              <img
+                src={`../../public/img/portfolio-gomission.png`}
+                className="w-full h-full object-cover"
+                alt=""
+              />
             </div>
             <div className="imageItem  row-span-3 h-[180]">
-              <img src="" className="w-full h-full object-cover" alt="" />
+              <img
+                src={`../../public/img/portfolio-tazamia.png`}
+                className="w-full h-full object-cover"
+                alt=""
+              />
             </div>
             <div className="imageItem row-span-6 h-[180]">
-              <img src="" className="w-full h-full object-cover" alt="" />
+              <img
+                src={`../../public/img/portfolio-ottm.png`}
+                className="w-full h-full object-cover"
+                alt=""
+              />
             </div>
             <div className="imageItem row-span-2 h-[180]">
-              <img src="" className="w-full h-full object-cover" alt="" />
+              <img
+                src={`../../public/img/portfolio-ftc.png`}
+                className="w-full h-full object-cover"
+                alt=""
+              />
             </div>
             <div className="imageItem row-span-2 h-[180]">
               <img src="" className="w-full h-full object-cover" alt="" />
             </div>
             <div className="imageItem row-span-3 h-[180]">
-              <img src="" className="w-full h-full object-cover" alt="" />
+              <img
+                rc={`../../public/img/portfolio-fca.png`}
+                className="w-full h-full object-cover"
+                alt=""
+              />
             </div>
             <div className="imageItem row-span-2 h-[180]">
-              <img src="" className="w-full h-full object-cover" alt="" />
+              <img
+                src={`../../public/img/portfolio-phixgenomics.png`}
+                className="w-full h-full object-cover"
+                alt=""
+              />
             </div>
             <div className="imageItem row-span-2 h-[180]">
               <img src="" className="w-full h-full object-cover" alt="" />
