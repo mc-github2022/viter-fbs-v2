290cf9f6ff51d5712235893ec78af8dd02bc8dfd
Jhonny Dichoso <jhonny.dichoso@frontlinebusiness.com.ph>
2024-10-11 15:41:01 +0800

    continuing studies content

--- src/components/pages/website/learningCenterSolution/continuingStudies/ConStudBanner.jsx
+++ src/components/pages/website/learningCenterSolution/continuingStudies/ConStudBanner.jsx
@@ -15,19 +15,17 @@ const ConStudBanner = () => {
           <div className="wrapper grid grid-cols-2 place-items-center h-[80vh] md:h-[70vh] transition-all w-full">
             <div className="text-left">
               <h2 className="text-[45px] leading-[1.1] mb-8 text-light font-light">
+                Unlock Your Potential <br />
+                with our
                 <span className="text-light font-semibold">
-                  College On-the-job
+                  &nbsp;Continuing Study Program
                 </span>
-                <br />
-                with industry experience practical, work-related skills
               </h2>
               <p className="text-light mb-10">
-                Partner with us for cutting-edge on-the-job training programs
-                that equip aspiring IT and Accounting professionals with
-                real-world skills, bridging academic learning with practical
-                workplace experience. Our program can accommodate students with
-                required internship hours ranging from 150 to 600, ensuring
-                flexibility and comprehensive skills development.
+                Empowering young professionals, fresh graduates, and career
+                shifters with essential technical skills in frontend
+                development, backend development, and Accounting through
+                hands-on training and real-world experience.
               </p>
               <a href="#" className="btn bg-transparent text-light border-2">
                 SCHEDULE A DEMO
