0ed54bd3c81c85c64e23eecaa748f5e16e78c190
Jhonny Dichoso <jhonny.dichoso@frontlinebusiness.com.ph>
2024-10-22 13:33:13 +0800

    VA content

--- src/components/pages/website/virtualSolution/vaResearch/data.jsx
+++ src/components/pages/website/virtualSolution/vaResearch/data.jsx
@@ -14,8 +14,8 @@ export const banner = [
 
 export const sectionPartnersTitle = [
   {
-    subTitle: "Clients who trusted their",
-    mainTitle: "growth with us.",
+    subTitle: "Clients Who Trusted Their",
+    mainTitle: "Growth With Us.",
   },
 ];
 
@@ -33,8 +33,8 @@ export const sectionPartnersLogos = [
 
 export const webAppOverview = [
   {
-    subtitle: "Here's why we're the",
-    mainTitle: "right choice for your VA needs.",
+    subtitle: "Here's Why We're The",
+    mainTitle: "Right Choice for Your VA Needs.",
     webAppImage: "vaResearch.jpg",
     btnText: "Schedule A Demo",
     btnDownloadText: "Download Proposal",
