a24706825997f17db950d185566d7dd842d7fe43
Jhonny Dichoso <jhonny.dichoso@frontlinebusiness.com.ph>
2024-10-23 16:05:47 +0800

    lcs img optimization

--- src/components/pages/website/learningCenterSolution/continuingStudies/ConStudApplyNow.jsx
+++ src/components/pages/website/learningCenterSolution/continuingStudies/ConStudApplyNow.jsx
@@ -49,7 +49,7 @@ const ConStudApplyNow = () => {
             <div className="bgImage w-full absolute top-0 left-0 md:left-auto md:w-[68%] md:right-0 md:h-full">
               <img
                 // src={`${devBaseImgUrl}/ojt-accepted.jpg`}
-                src="../../public/img/ojt-accepted.jpg"
+                src="../../public/img/ojt-accepted.webp"
                 className="w-full h-full object-cover object-top"
                 alt=""
               />
