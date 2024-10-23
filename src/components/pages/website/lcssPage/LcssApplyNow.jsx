0441f44da62471b366ffba9d3d5ebe23881b69eb
Jhonny Dichoso <jhonny.dichoso@frontlinebusiness.com.ph>
2024-10-23 13:00:05 +0800

    college ojt img optimization

--- src/components/pages/website/lcssPage/LcssApplyNow.jsx
+++ src/components/pages/website/lcssPage/LcssApplyNow.jsx
@@ -44,7 +44,7 @@ const LcssApplyNow = () => {
             <div className="bgImage w-full absolute top-0 left-0 md:left-auto md:w-[68%] md:right-0 md:h-full">
               <img
                 // src={`${devBaseImgUrl}/ojt-accepted.jpg`}
-                src="../../public/img/ojt-accepted.jpg"
+                src="../../public/img/ojt-accepted.webp"
                 className="w-full h-full object-cover object-top"
                 alt=""
               />
