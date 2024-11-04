8b42459bd98a7ab6b803d18129dcfc0b2919c8d4
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-11-05 07:39:57 +0800

    updated

--- src/components/pages/website/home/Process.jsx
+++ src/components/pages/website/home/Process.jsx
@@ -21,35 +21,52 @@ const Process = () => {
             <div className="grid place-items-center">
               <div className="md:grid grid-cols-4">
                 <div className="processItem md:text-center md:translate-y-[-50px] lg:translate-y-[-100px] mb-6">
-                  <p className="text-primary font-bold text-lg">
-                    We Collaborate
-                  </p>
-                  <p>
-                    with our clients to understand their specific talent needs
-                    and project requirements.
-                  </p>
+                  <div>
+                    <img
+                      src="../../public/img/wecollaborate.png"
+                      className=""
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <p className="text-primary font-bold text-lg">
+                      We Collaborate
+                    </p>
+                    <p>
+                      with our clients to understand their specific talent needs
+                      and project requirements.
+                    </p>
+                  </div>
                 </div>
                 <div className="processItem md:text-center mb-6">
-                  <p className="text-primary font-bold text-lg">We Deliver</p>
-                  <p>
-                    skilled professionals tailored to meet the client's demands,
-                    ensuring the right fit for their business.
-                  </p>
+                  <div>
+                    <p className="text-primary font-bold text-lg">We Deliver</p>
+                    <p>
+                      skilled professionals tailored to meet the client's
+                      demands, ensuring the right fit for their business.
+                    </p>
+                  </div>
                 </div>
                 <div className="processItem md:text-center md:translate-y-[-50px] lg:translate-y-[-100px] mb-6">
-                  <p className="text-primary font-bold text-lg">We Manage</p>
-                  <p>
-                    the ongoing performance and development of the provided
-                    talents, ensuring seamless integration and success.
-                  </p>
+                  <div>
+                    <p className="text-primary font-bold text-lg">We Manage</p>
+                    <p>
+                      the ongoing performance and development of the provided
+                      talents, ensuring seamless integration and success.
+                    </p>
+                  </div>
                 </div>
                 <div className="processItem md:text-center">
-                  <p className="text-primary font-bold text-lg">We Give back</p>
-                  <p>
-                    to the ministry and work for the Kingdom of God, aligning
-                    our business practices with our faith and commitment to a
-                    higher purpose.
-                  </p>
+                  <div>
+                    <p className="text-primary font-bold text-lg">
+                      We Give back
+                    </p>
+                    <p>
+                      to the ministry and work for the Kingdom of God, aligning
+                      our business practices with our faith and commitment to a
+                      higher purpose.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
