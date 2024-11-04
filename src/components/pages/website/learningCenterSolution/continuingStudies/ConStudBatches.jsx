e862264c3b9a5bc490b6b77dd47c250b57edcdcc
Jhonny Dichoso <jhonny.dichoso@frontlinebusiness.com.ph>
2024-11-05 07:34:14 +0800

    faded background on batches cards

--- src/components/pages/website/learningCenterSolution/continuingStudies/ConStudBatches.jsx
+++ src/components/pages/website/learningCenterSolution/continuingStudies/ConStudBatches.jsx
@@ -182,7 +182,7 @@ const ConStudBatches = () => {
                               </h4>
                             </div>
                           </div>
-                          <div className="bottomGradient bg-gradient-to-t from-dark to-blue-500 h-[200px] w-full absolute bottom-0 block rounded-bl-xl rounded-br-xl"></div>
+                          <div className="bottomGradient bg-gradient-to-t from-[#000] to-blue-500 h-[100%] w-full absolute bottom-0 block rounded-bl-xl rounded-br-xl"></div>
                         </div>
                       </a>
                     </div>
