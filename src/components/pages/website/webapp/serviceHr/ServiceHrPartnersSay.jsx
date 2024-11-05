56ee085b5a857ab363d08fa35b33caa9ab900247
abtGrgn <ansbert.gregana@frontlinebusiness.com.ph>
2024-11-05 13:32:23 +0800

    pricing ui updates

--- src/components/pages/website/webapp/serviceHr/ServiceHrPartnersSay.jsx
+++ src/components/pages/website/webapp/serviceHr/ServiceHrPartnersSay.jsx
@@ -120,7 +120,7 @@ const ServiceHrPartnersSay = () => {
 
   return (
     <>
-      <section className="ServiceHrPartnersSay pb-10 md:pb-20 ">
+      <section className="ServiceHrPartnersSay pb-10 md:py-20 ">
         <div className="customContainer">
           {IndtestimonialData?.data.length > 0 && (
             <div className="">
@@ -143,7 +143,7 @@ const ServiceHrPartnersSay = () => {
                   ) {
                     return (
                       <div key={key} className="md:h-fit">
-                        <div className="testimonialItem bg-customGray lg:grid lg:grid-cols-[_2fr_1fr] items-center md:top-[50%] py-10 px-10 md:px-20 md:pt-16 pb-9 mb-5 rounded-xl md:w-ful mx-auto relative">
+                        <div className="testimonialItem bg-customGray lg:grid lg:grid-cols-[_2fr_1fr] items-center md:top-[50%] py-10 px-10 md:px-20 md:pt-16 pb-9 mb-5 rounded-xl md:w-[80%] mx-auto relative">
                           <div className="theMessage  relative">
                             <div className="absolute top-[-30px] left-[-40px]">
                               <img
