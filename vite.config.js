4bb1794eed463f814aaec04d3af61a2b71763e14
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-11-12 14:51:31 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // local
-  // base: "/newsite", // online
+  // base: "/", // local
+  base: "/newsite", // online
   build: {
     chunkSizeWarningLimit: 15000,
   },
