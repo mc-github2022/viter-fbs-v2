7707c6d84dec2549003de1457334652ca7b982f6
peegeetr <patrickgreyes@gmail.com>
2024-11-12 13:16:08 +0800

    updating version

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  // base: "/", // local
+  base: "/newwsite", // online
   build: {
     chunkSizeWarningLimit: 4000,
   },
