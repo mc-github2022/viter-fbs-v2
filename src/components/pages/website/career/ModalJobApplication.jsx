23d23288ee5384cbda968705ff1ac6cae001e391
FBS-ENAGIC-05 <lourenisobel18@gmail.com>
2024-11-19 14:37:26 +0800

    job modal updated

--- src/components/pages/website/career/ModalJobApplication.jsx
+++ src/components/pages/website/career/ModalJobApplication.jsx
@@ -67,6 +67,7 @@ const ModalJobApplication = ({ setModalJob, jobTitle }) => {
     client_phone: "",
     client_message: "",
     client_file: "",
+    client_message_subject: "",
     formTitle: `Job Application: ${jobTitle}`,
   };
 
