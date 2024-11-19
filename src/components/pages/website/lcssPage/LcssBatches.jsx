19c0ba252fdb4aaa1f1aac5af62bed235607533e
FBS-ENAGIC-05 <lourenisobel18@gmail.com>
2024-11-19 09:02:19 +0800

    slider batch updated

--- src/components/pages/website/lcssPage/LcssBatches.jsx
+++ src/components/pages/website/lcssPage/LcssBatches.jsx
@@ -174,7 +174,7 @@ const LcssBatches = () => {
                         <div key={key} className="mb-[3rem]">
                           <a onClick={() => handleModalBatch(item)}>
                             <div
-                              className="blogItem addShadow bg-center bg-cover h-[400px] min-w-[300px] max-w-[300px] md:min-w-[350px] md:max-w-[350px] flex items-end relative rounded-xl 
+                              className="blogItem addShadow bg-center bg-cover h-[400px] min-w-[280px] max-w-[280px] md:min-w-[350px] md:max-w-[350px] flex items-end relative rounded-xl 
                             grayscale hover:grayscale-0 transition-all group cursor-pointer"
                               style={{
                                 backgroundImage: `url(${devBaseImgUrl}/${firstImage})`,
@@ -222,7 +222,7 @@ const LcssBatches = () => {
                           <a onClick={() => handleModalBatch(item)}>
                             <div
                               className="blogItem addShadow bg-center bg-cover h-[400px] 
-                              w-full md:w-[350px] flex items-end relative rounded-xl 
+                              min-w-[280px] max-w-[280px] md:min-w-[350px] md:max-w-[350px] flex items-end relative rounded-xl 
                               grayscale hover:grayscale-0 transition-all group cursor-pointer "
                               style={{
                                 backgroundImage: `url(${devBaseImgUrl}/${firstImage})`,
