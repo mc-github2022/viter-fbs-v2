f9f3f8687648ee916c92bd7de194a4dade19d7b1
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-11-19 11:25:11 +0800

    emailupdated

--- rest/v1/controllers/developer/sending-email/sending-email.php
+++ rest/v1/controllers/developer/sending-email/sending-email.php
@@ -28,11 +28,11 @@ if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
     if(str_contains($title,"career")){
         $emailReceiver = ["kennie.deriquito@frontlinebusiness.com.ph","rhoda.beloso@frontlinebusiness.com.ph","jhonny.dichoso@frontlinebusiness.com.ph"];
         // $emailReceiver = ["mark.bumagat@frontlinebusiness.com.ph"];
-    }elseif(str_contains($title,"lcss") || str_contains($title,"immersion") || str_contains($title,"conStud")){
+    }elseif(str_contains($title,"College OJT") || str_contains($title,"Work Immersion") || str_contains($title,"Continuing Study")){
         $emailReceiver = ["herlyn.torres@frontlinebusiness.com.ph","thea.consignado@frontlinebusiness.com.ph","jhonny.dichoso@frontlinebusiness.com.ph"];
         // $emailReceiver = ["markbumagat87@gmail.com"];
     }else{
-        $emailReceiver = ["jhonny.dichoso@frontlinebusiness.com.ph","mktg@frontlinebusiness.com.ph"];
+        $emailReceiver = ["jhonny.dichoso@frontlinebusiness.com.ph","marketing@frontlinebusiness.com.ph"];
         // $emailReceiver = ["markbumagat87@gmail.com"];
     }
     
