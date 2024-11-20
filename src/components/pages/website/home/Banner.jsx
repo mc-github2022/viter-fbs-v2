269a7ba9b8804ce35de6ca51ef4056fd18639d7e
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-11-20 14:31:56 +0800

    added banner loader

--- src/components/pages/website/home/Banner.jsx
+++ src/components/pages/website/home/Banner.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BannerSlider from "./BannerSlider";
 
 const Banner = () => {
@@ -6,8 +6,7 @@ const Banner = () => {
     <>
       <section
         id="HomeBanner"
-        className="banner h-[100vh] place-content-center"
-      >
+        className="banner h-[100vh] place-content-center">
         <div className="pt-[43px] md:pt-[95px]">
           <BannerSlider />
         </div>
---
+++ src/components/pages/website/home/BannerSliderLoader.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const BannerSliderLoader = () => {
+  return (
+    <>
+      <div
+        className={`banner bg-cover  bg-center py-[30px] h-[100%] grid place-content-center`}>
+        <div>
+          <div className="animate-pulse space-y-2 mb-5">
+            <div className="h-2 w-[170px] md:w-[300px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[170px] md:w-[300px] bg-[#f5f3f3] rounded mx-auto"></div>
+          </div>
+          <div className="animate-pulse space-y-2 mb-5">
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+          </div>
+          <div className="animate-pulse space-y-2 mb-8">
+            <div className="h-2 w-[250px] md:w-[500px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[250px] md:w-[500px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[250px] md:w-[500px] bg-[#f5f3f3] rounded mx-auto"></div>
+          </div>
+          <div className="animate-pulse space-y-2 mb-12">
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[280px] md:w-[800px] bg-[#f5f3f3] rounded mx-auto"></div>
+          </div>
+          <div className="animate-pulse space-y-2 mb-8">
+            <div className="h-2 w-[250px] md:w-[500px] bg-[#f5f3f3] rounded mx-auto"></div>
+            <div className="h-2 w-[250px] md:w-[500px] bg-[#f5f3f3] rounded mx-auto"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BannerSliderLoader;
