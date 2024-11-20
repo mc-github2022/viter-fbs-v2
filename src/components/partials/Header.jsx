390c84781ce9a015159ea82206e1a0d8c2bcce78
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-11-20 11:30:59 +0800

    updated mega menu

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -52,11 +52,9 @@ const Header = ({ pageName }) => {
     <>
       <section
         id="header"
-        className="header py-2 md:py-0 bg-light fixed w-full z-[99] lg:z-[999] shadow-md"
-      >
+        className="header py-2 md:py-0 bg-light fixed w-full z-[99] lg:z-[999] shadow-md">
         <div
-          className={`${toggleNav ? "overflow-y-hidden" : ""} customContainer`}
-        >
+          className={`${toggleNav ? "overflow-y-hidden" : ""} customContainer`}>
           <div className="wrapper flex justify-between items-center">
             <div className="theLogo">
               <Link to={`${devNavUrl}/`}>
@@ -70,16 +68,14 @@ const Header = ({ pageName }) => {
             <div
               className={`${
                 toggleNav ? "active" : ""
-              } theNav flex justify-end md:justify-between items-center `}
-            >
+              } theNav flex justify-end md:justify-between items-center `}>
               <ul className="md:flex  [&>li]:flex [&>li]:items-center md:ml-auto lg:m-0 h-screen md:h-[96px]">
                 <li>
                   <Link
                     to={`${devNavUrl}/`}
                     className={`${
                       pageName === "home" ? "text-primary !cursor-default" : ""
-                    }`}
-                  >
+                    }`}>
                     Home
                   </Link>
                 </li>
@@ -89,8 +85,7 @@ const Header = ({ pageName }) => {
                     onClick={handleToggleMenu}
                     className={`${
                       toggleMenu ? "text-primary" : ""
-                    } flex items-center gap-2`}
-                  >
+                    } flex items-center gap-2`}>
                     Services
                     <BiSolidDownArrow
                       className={`${
@@ -106,8 +101,7 @@ const Header = ({ pageName }) => {
                       toggleWhyUs ? "text-primary" : ""
                     } flex items-center gap-2`}
                     onClick={handdleWhyUs}
-                    ref={ref}
-                  >
+                    ref={ref}>
                     Why FBS?
                     <BiSolidDownArrow
                       className={`${
@@ -118,10 +112,9 @@ const Header = ({ pageName }) => {
                   <ul
                     className={`${
                       toggleWhyUs
-                        ? "md:!absolute md:!top-[96px] md:!w-[180px] md:addShadow !bg-customGray [&>li]:my-2 lg:[&>li]:my-2 py-0 md:p-[20px]  !top-12 pl-20 md:pl-[20px] transition-all md:!bg-light"
+                        ? "md:!absolute md:!top-[96px] md:!w-[180px] md:addShadow !bg-customGray [&>li]:my-2 lg:[&>li]:my-2 py-0 md:p-[20px]  !top-12 pl-[2.75rem] md:pl-[20px] transition-all md:!bg-light"
                         : "hidden"
-                    } left-0  text-sm p-5 md:rounded-bl-xl md:rounded-br-xl`}
-                  >
+                    } left-0  text-sm p-5 md:rounded-bl-xl md:rounded-br-xl`}>
                     <li>
                       <Link
                         // className="!p-0 hover:text-primary"
@@ -130,8 +123,7 @@ const Header = ({ pageName }) => {
                           pageName === "whyWorkWithUs"
                             ? "!p-0 text-primary !cursor-default"
                             : "!p-0 hover:text-primary"
-                        }`}
-                      >
+                        }`}>
                         Why Work With Us
                       </Link>
                     </li>
@@ -142,8 +134,7 @@ const Header = ({ pageName }) => {
                           pageName === "events&Activities"
                             ? "!p-0 text-primary !cursor-default"
                             : "!p-0 hover:text-primary"
-                        }`}
-                      >
+                        }`}>
                         Events & Activities
                       </Link>
                     </li>
@@ -154,8 +145,7 @@ const Header = ({ pageName }) => {
                           pageName === "career"
                             ? "!p-0 text-primary !cursor-default"
                             : "!p-0 hover:text-primary"
-                        }`}
-                      >
+                        }`}>
                         Career
                       </Link>
                     </li>
@@ -165,8 +155,7 @@ const Header = ({ pageName }) => {
                   <a
                     href="#"
                     onClick={handleModalContact}
-                    className="btn bg-gradient-to-r hover:duration-500 hover:bg-gradient-to-r text-light rounded-full  from-secondary to-secondary hover:to-primary "
-                  >
+                    className="btn bg-gradient-to-r hover:duration-500 hover:bg-gradient-to-r text-light rounded-full  from-secondary to-secondary hover:to-primary ">
                     GET STARTED
                   </a>
                 </div>
@@ -175,8 +164,7 @@ const Header = ({ pageName }) => {
             <div className="lg:w-[200px] lg:flex justify-center hidden md:block">
               <button
                 onClick={handleModalContact}
-                className="btn bg-gradient-to-r hover:duration-500 hover:bg-gradient-to-r text-light my-5  lg:block rounded-full  from-secondary to-secondary hover:to-primary "
-              >
+                className="btn bg-gradient-to-r hover:duration-500 hover:bg-gradient-to-r text-light my-5  lg:block rounded-full  from-secondary to-secondary hover:to-primary ">
                 GET STARTED
               </button>
             </div>
@@ -185,8 +173,7 @@ const Header = ({ pageName }) => {
               onClick={handdleToggle}
               className={`${
                 toggleNav ? "active" : ""
-              } toggleNav block md:hidden relative w-[20px] h-[20px] ml-auto`}
-            >
+              } toggleNav block md:hidden relative w-[20px] h-[20px] ml-auto`}>
               <span></span>
               <span></span>
               <span></span>
