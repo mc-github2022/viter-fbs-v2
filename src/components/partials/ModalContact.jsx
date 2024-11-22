8ebba128610e348473364f4d9cb8e3614a1783c0
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-11-22 15:09:56 +0800

    modal contact lcss

--- src/components/partials/ModalContact.jsx
+++ src/components/partials/ModalContact.jsx
@@ -88,7 +88,7 @@ const ModalContact = ({
     <>
       <div
         onClick={handleClose}
-        className="ModalContact fixed w-full h-screen top-0 bg-dark bg-opacity-70 z-[9999] grid place-items-center backdrop-blur-sm overflow-auto py-6 md:py-0"
+        className="ModalContact fixed w-full h-screen px-4 top-0 bg-dark bg-opacity-70 z-[9999] grid place-items-center backdrop-blur-sm overflow-auto py-6 md:py-0"
       >
         <div
           onClick={(e) => {
