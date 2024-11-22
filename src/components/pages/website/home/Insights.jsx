7c75231049654b885c9bb0841cbdce0cdd34fc37
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2024-11-22 15:00:58 +0800

    color gradient updated

--- src/components/pages/website/home/Insights.jsx
+++ src/components/pages/website/home/Insights.jsx
@@ -182,7 +182,7 @@ const Insights = () => {
                                 </h4>
                               </div>
                             </div>
-                            <div className="bottomGradient bg-gradient-to-t from-[#000] to-blue-500 h-[300px] w-full absolute bottom-0 block rounded-bl-xl rounded-br-xl"></div>
+                            <div className="bottomGradient bg-gradient-to-t from-[#000] !to-[transparent] h-[300px] w-full absolute bottom-0 block rounded-bl-xl rounded-br-xl"></div>
                           </div>
                         </Link>
                       </div>
@@ -214,7 +214,7 @@ const Insights = () => {
                               </h4>
                             </div>
                           </div>
-                          <div className="bottomGradient bg-gradient-to-t from-[#000] to-blue-500 h-[300px] w-full absolute bottom-0 block rounded-bl-xl rounded-br-xl"></div>
+                          <div className="bottomGradient bg-gradient-to-t from-[#000] !to-[transparent] h-[300px] w-full absolute bottom-0 block rounded-bl-xl rounded-br-xl"></div>
                         </div>
                       </Link>
                     </div>
