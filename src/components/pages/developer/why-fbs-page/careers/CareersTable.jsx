df1af562658a21bb0d777cf9a51c5bd4eec5f1c5
FBS-ENAGIC-05 <lourenisobel18@gmail.com>
2024-11-27 15:59:03 +0800

    career table updated

--- src/components/pages/developer/why-fbs-page/careers/CareersTable.jsx
+++ src/components/pages/developer/why-fbs-page/careers/CareersTable.jsx
@@ -52,8 +52,8 @@ const CareersTable = ({ setItemEdit }) => {
               <th>employee classification</th>
               <th>Mode of Work</th>
               <th>Job Status</th>
-              <th className="">Job Overview</th>
-              <th className="w-[15rem]">Job Description</th>
+              <th className="w-[18rem]">Job Overview</th>
+              <th className="w-[18rem]">Job Description</th>
               <th>Image</th>
               <th className="text-right">Actions</th>
             </tr>
