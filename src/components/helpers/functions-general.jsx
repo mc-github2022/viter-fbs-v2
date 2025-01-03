f1c6af4f6416f3a7f08c176e1493ddabcb644b78
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2025-01-03 09:02:50 +0800

    updated X

--- src/components/helpers/functions-general.jsx
+++ src/components/helpers/functions-general.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { StoreContext } from "../store/StoreContext";
 
 // local url
-export const urlPathFBSMarketingWebsite =
-  "http://localhost/react-vite/viter-fbs-v2";
-export const imgUrlPathFBSMarketingWebsite =
-  "http://localhost/react-vite/viter-fbs-v2/public/img";
+// export const urlPathFBSMarketingWebsite =
+//   "http://localhost/react-vite/viter-fbs-v2";
+// export const imgUrlPathFBSMarketingWebsite =
+//   "http://localhost/react-vite/viter-fbs-v2/public/img";
 
 // Online Demo url
 // export const urlPathFBSMarketingWebsite =
@@ -14,9 +14,9 @@ export const imgUrlPathFBSMarketingWebsite =
 //   "http://localhost/react-vite/viter-fbs-v2/public/img";
 
 // // Online Demo url
-// export const urlPathFBSMarketingWebsite = "https://frontlinebusiness.com.ph";
-// export const imgUrlPathFBSMarketingWebsite =
-//   "http://frontlinebusiness.com.ph/img";
+export const urlPathFBSMarketingWebsite = "https://frontlinebusiness.com.ph";
+export const imgUrlPathFBSMarketingWebsite =
+  "http://frontlinebusiness.com.ph/img";
 
 export const devApiUrl = `${urlPathFBSMarketingWebsite}/rest`;
 export const devBaseUrl = `${urlPathFBSMarketingWebsite}`;
@@ -31,11 +31,11 @@ export const UrlDeveloper = "developer";
 export const devKey =
   "$2a$12$47wDvbLInZif/PVS8B6P3.7WxyJvUpBzZAWCsnWJUKq3nrn4qgmeO";
 
-//reCAPTCHA site key TEST
-export const siteKey = "6LeUuBgqAAAAAKDIsrmFwuV8ocqZusI7fB_69Gva";
+// reCAPTCHA site key TEST
+// export const siteKey = "6LeUuBgqAAAAAKDIsrmFwuV8ocqZusI7fB_69Gva";
 
 // reCAPTCHA site key PRODUCTION
-// export const siteKey = "";
+export const siteKey = "";
 
 // formatting date and time
 export const setTimeZone = "Asia/Taipei";
