a7375c3bb238747a1722bc0445a2547f1d2f5ab6
FBS-ENAGIC-05 <lourenisobel18@gmail.com>
2025-01-10 13:41:36 +0800

    revision added

--- rest/v1/recaptcha/verify-recaptcha.php
+++ rest/v1/recaptcha/verify-recaptcha.php
@@ -12,7 +12,7 @@ function verifyRecaptcha($res)
     $post_data =
         [
             'secret' => '', //<--- your reCaptcha secret key PRODUCTION
-            // 'secret' => '6LeUuBgqAAAAAOs2u2mDWvs9v5uCx-5V3bI7czOc', //<--- your reCaptcha secret key TEST
+            // 'secret' => '', //<--- your reCaptcha secret key TEST
             'response' => $res
         ];
 
