0461c544a7280008a314e5f458c430ad971d0c43
FBS-ENAGIC-05 <lourenisobel18@gmail.com>
2025-02-05 08:17:28 +0800

    email log is updated

--- src/components/pages/developer/subscribers/email-log/EmailLogStatus.jsx
+++ src/components/pages/developer/subscribers/email-log/EmailLogStatus.jsx
@@ -6,9 +6,9 @@ const EmailLogStatus = ({ text }) => {
       <span
         className={` ${
           text === "Sent"
-            ? "bg-[#dcfce7] text-[#166534]  border border-[#bbf7d0] text-xs font-medium mr-2 px-2 py-0.5 rounded-md"
+            ? "bg-[#dcfce7] text-[#166534]  text-xs font-medium mr-2 px-2 py-0.5 rounded-md"
             : text === "Failed"
-            ? "bg-red-100 text-[#ef4444] border border-[#ef4444] text-xs font-medium mr-2 px-2 py-0.5 rounded-md"
+            ? "bg-red-100 text-[#ef4444]  text-xs font-medium mr-2 px-2 py-0.5 rounded-md"
             : ""
         }`}
       >
