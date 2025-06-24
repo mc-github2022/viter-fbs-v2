5b87cdd19bc1409abb2178dd76be29b7ec4c8347
Louren Rubico <louren.rubico@frontlinebusiness.com.ph>
2025-06-24 08:37:27 +0800

    newsletter template is updated

--- rest/v1/notification/template/subscriber-newsletter.php
+++ rest/v1/notification/template/subscriber-newsletter.php
@@ -21,34 +21,10 @@ function getHtmlSendMessage(
    
       <div
         class="footer"
-        style="
-        padding: 16px 0px;
-     margin-bottom: 0;
-        text-align: center;
-        background-color: #fff;">
-        <div style="text-align: center; line-height: 1; width: 420px; margin: 0 auto;">
-            <div ><p style="font-size: 16px;  style="
-        padding-top: 16px;> Want to change how you receive these emails? <br></p> <small >
+        style="padding: 16px 0px; margin-bottom: 0; text-align: center; background-color: #fff; margin: 0 auto; max-width: 700px; width: 100%; line-height: 1;">
+          <div ><p style="font-size: 16px; padding-top: 16px;"> Want to change how you receive these emails? <br></p> <small >
             <a style="font-size: 14px;  text-decoration: underline; font-family: Arial, Helvetica, sans-serif" href="' . $ROOT_DOMAIN . '' . $unsubscribe_link . '?key=' . $newKey . '">Unsubscribe</a></small>
           </div>
-          <p style="font-size: 14px; padding: 0 10px">
-          <small>
-            Having issues with the link? Try to paste this text on your browser
-            URL:  <br />
-            <a
-              target="_blank"
-              rel="nofollow"
-              style="
-                font-size: 12px;
-                font-family: Arial, Helvetica, sans-serif;
-                text-decoration: none;
-                color: #505050;
-              "
-              >' . $ROOT_DOMAIN . '' . $unsubscribe_link . '?key=' . $newKey . '</a
-      >
-      </small>
-      </p>
-              </div>
       </div>
     </body>
   </html>
